07608063fa08fa6fa86c67378f817858c629fe98
Jolop1513 <jolop1513@gmail.com>
2022-05-26 14:40:46 -0500

    update

--- todoapp.py
+++ todoapp.py
@@ -5,6 +5,62 @@ app = Flask(__name__, template_folder='templates')
 
 datosF = []
 
+"""
+def buscarArchivo():
+    try:
+        archivo = pickle.load(open("dict.pickle","rb"))
+    except:
+        pickle.dump(datosF, open("dict.pickle","wb"))
+        
+buscarArchivo()
+
+
+# ************************************************************
+class Tarea:
+    # Constructor de clase
+    def __init__(self, id, titulo, correo, prioridad):
+        self.titulo = titulo
+        self.correo = correo
+        self.prioridad = prioridad
+        self.id = id
+        print('Se ha creado la tarea:',self.titulo)
+
+    def __str__(self):
+        return '{} ({})'.format(self.id, self.titulo, self.correo, self.prioridad)
+class ListaTareas:
+    # Constructor de clase
+    def __init__(self):
+        self.cargar()
+
+    def agregar(self,p):
+        self.datosF.append(p)
+        self.guardar()
+
+    def mostrar(self):
+        if len(self.datosF) == 0:
+            print("La lista está vacía")
+            return
+        for p in self.datosF:
+            print(p)
+
+    def cargar(self):
+        fichero = open('dict.pickle', 'ab+')
+        fichero.seek(0)
+        try:
+            self.datosF = pickle.load(fichero)
+        except:
+            print("El fichero está vacío")
+        finally:
+            fichero.close()
+            print("Se han cargado {} tareas".format(len(self.datosF)))
+
+    def guardar(self):
+        fichero = open('dict.pickle', 'wb')
+        pickle.dump(self.datosF, fichero)
+        fichero.close()
+# ************************************************************
+"""
+
 @app.route('/')
 #contenedor para llamar a index.html y los datos registrados en la ruta principal
 def index():
